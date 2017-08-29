5b03256a1f547f94acb742f8e42d29be41418d3f
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-08-29 14:47:24 -0400

    javascripting

---
+++ for-loop.js
@@ -0,0 +1,25 @@
+var total = 0;
+var limit = 10;
+for (var i = 0; i < limit; i++) {
+  total += i;
+}
+
+console.log(total);
+
+
+
+
+
+
+
+// Create a for loop with a variable i starting at 0 and increasing by 1 each
+//  time through the loop. The loop should run as long as i is less than
+//  limit.
+//
+//  On each iteration of the loop, add the number i to the total variable. To
+//  do this, you can use this statement:
+//
+//     total += i;
+//
+//  After the for loop, use console.log() to print the total variable to the
+//  terminal.
